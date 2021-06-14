4414027a7edf6fe78db557eef1cb01b05ed1cb85
shofizone <shofizone@gmail.com>
2021-06-14 17:01:23 +0600

    migrate to null -safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,8 +69,9 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _takePhoto() async {
-    ImagePicker.pickImage(source: ImageSource.camera)
-        .then((File recordedImage) {
+    ImagePicker.platform
+        .pickImage(source: ImageSource.camera)
+        .then((PickedFile recordedImage) {
       if (recordedImage != null && recordedImage.path != null) {
         setState(() {
           firstButtonText = 'saving in progress...';
@@ -86,8 +87,9 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _recordVideo() async {
-    ImagePicker.pickVideo(source: ImageSource.camera)
-        .then((File recordedVideo) {
+    ImagePicker.platform
+        .pickVideo(source: ImageSource.camera)
+        .then((recordedVideo) {
       if (recordedVideo != null && recordedVideo.path != null) {
         setState(() {
           secondButtonText = 'saving in progress...';
@@ -174,11 +176,10 @@ class _ScreenshotWidgetState extends State<ScreenshotWidget> {
       await capturedFile.writeAsBytes(pngBytes);
       print(capturedFile.path);
 
-      await GallerySaver.saveImage(capturedFile.path)
-          .then((value) {
-            setState(() {
-              screenshotButtonText = 'screenshot saved!';
-            });
+      await GallerySaver.saveImage(capturedFile.path).then((value) {
+        setState(() {
+          screenshotButtonText = 'screenshot saved!';
+        });
       });
     } catch (e) {
       print(e);
